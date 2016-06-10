f67af103637f39c57d5b7ac609fbb86b465ebe97
Bryce Evans <bryce-e@moving-picture.com>
2016-06-09 18:24:52 -0700

    ex14

--- ex14.c
+++ ex14.c
@@ -2,17 +2,18 @@
 #include <ctype.h>
 
 // forward declarations
-int can_print_it(char ch);
+//int can_print_it(char ch);
 void print_letters(char arg[]);
 
-void print_arguments(int argc, char *argv[])
-{
-    int i = 0;
-
-    for(i = 0; i < argc; i++) {
-        print_letters(argv[i]);
-    }
-}
+// dont need this func, can loop in main
+// void print_arguments(int argc, char *argv[])
+// {
+//     int i = 0;
+// 
+//     for(i = 0; i < argc; i++) {
+//         print_letters(argv[i]);
+//     }
+// }
 
 void print_letters(char arg[])
 {
@@ -21,10 +22,26 @@ void print_letters(char arg[])
     for(i = 0; arg[i] != '\0'; i++) {
         char ch = arg[i];
 
-        if(can_print_it(ch)) {
+        //if(can_print_it(ch)) {
+        if (isalpha(ch) || isblank(ch)) {
             printf("'%c' == %d ", ch, ch);
         }
     }
 
     printf("\n");
+}
+
+// do we need this func?
+//int can_print_it(char ch)
+//{
+//    return isalpha(ch) || isblank(ch);
+//}
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    for(i = 0; i < argc; i++) {
+        print_letters(argv[i]);
+    }
+    return 0;
 }
