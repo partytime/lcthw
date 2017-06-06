6f27ab93ab34ad16707bf4c1282e88608816e4ed
Bryce Evans <bryceevans@gmail.com>
2017-06-05 22:41:52 -0700

    complete 1 to six, resume @ 7

---
+++ ex1/extra/ex1-extra.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int wtffffa, char *doesthismatter[])
+{
+    int distance = 100;
+
+    // this is also a comment
+    printf("You are %d miles away.\n", distance);
+    printf("line 1.\nline 2.\nline 3.\nline 4.\n", distance);
+
+    return 0;
+}
